f82e02b78f49323ae47ae18d44fd4b8bc84efed8
jaywiles <jay.wiles@gmail.com>
2019-11-30 15:55:48 -0600

    Updates

---
+++ precache-manifest.5b460c1ef561799ce80093fb1c93b2f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d52011b229e0004a6bc104df85052d0",
+    "url": "/jaywiles.github.io/index.html"
+  },
+  {
+    "revision": "cfbe6ed7e58af5e4db0a",
+    "url": "/jaywiles.github.io/static/css/main.d6fb4034.chunk.css"
+  },
+  {
+    "revision": "0e4946826cfc036f9cc4",
+    "url": "/jaywiles.github.io/static/js/2.5a1d806a.chunk.js"
+  },
+  {
+    "revision": "cfbe6ed7e58af5e4db0a",
+    "url": "/jaywiles.github.io/static/js/main.4f7e9e1b.chunk.js"
+  },
+  {
+    "revision": "49835ef725dffcdd7dea",
+    "url": "/jaywiles.github.io/static/js/runtime-main.efd6562b.js"
+  }
+]);
